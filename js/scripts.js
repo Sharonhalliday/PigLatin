94d24e4d9228f7a583c1c6afd196d3e1387fbb52
sharonhalliday <hallidaysharon5@gmail.com>
2024-01-28 23:50:41 -0800

    add javascript function for modified consonant

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,27 @@ function modifiedVowel(passage) {
     return wordArray.join(" ");
     
 }
+
+//for consonant
+function modifiedConsonant(word) {
+    let vowels = "aeiou";
+    let characterWords = word.split("");
+    let returnString = "";
+
+    for (let i = 0; i < characterWords.length; i++) {
+        if ("q" === characterWords[i].charAt(0).toLowerCase()) {
+            returnString = word.slice(2).concat(characterWords[i] + characterWords[i + 1] +
+                "ay");
+            return returnString;
+        } else if ("s" === characterWords[i].charAt(0).toLowerCase()) {
+            returnString = word.slice(3).concat(characterWords[i] + characterWords[i + 1] +
+                characterWords[i + 2] + "ay");
+            return returnString;
+        } else if (vowels.includes(characterWords[i].toLowerCase())) {
+            returnString = word.slice(i).concat(word.slice(0, i) + "ay");
+            return returnString;
+        }
+    }
+
+    return word;
+}
